2580a4bfb11d24b0c2bf95d0153dea1e189915c8
weew12 <2528566339@qq.com>
2026-03-26 19:11:45 +0800

    update add docs/4.计算机基础、底层/2.Unix 域套接字_Go 实战与性能优化.md

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -10,9 +10,9 @@
  * 特别的，请不要在两个配置文件中重复配置相同的项，当前文件的配置项会被覆盖
  */
 
-import {viteBundler} from '@vuepress/bundler-vite'
-import {defineUserConfig} from "vuepress";
-import {plumeTheme} from "../../vuepress-theme-plume/lib/node";
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defineUserConfig } from "vuepress";
+import { plumeTheme } from "../../vuepress-theme-plume/lib/node";
 import { baiduAnalyticsPlugin } from '@vuepress/plugin-baidu-analytics';
 
 export default defineUserConfig({
@@ -37,19 +37,19 @@ export default defineUserConfig({
     // ref https://v2.vuepress.vuejs.org/zh/reference/config.html#head
     head: [
         // 配置站点图标
-        ["link", {rel: "icon", type: "image/png", href: "/favicon.ico"}],
+        ["link", { rel: "icon", type: "image/png", href: "/favicon.ico" }],
         // 解决语雀防盗链 绕过
-        ["meta", {name: "referrer", content: "no-referrer"}],
+        ["meta", { name: "referrer", content: "no-referrer" }],
     ],
 
     // 设置站点要使用的打包工具
     // ref https://v2.vuepress.vuejs.org/zh/reference/config.html#bundler
     bundler: viteBundler(),
- 
+
     // 站点较大，页面数量较多时，不建议启用
     // 如果你将它设置为 true ，所有其它页面所需的文件都会被预拉取
     // ref https://v2.vuepress.vuejs.org/zh/reference/config.html#shouldprefetch
-    shouldPrefetch: false, 
+    shouldPrefetch: false,
 
     // 设置站点要使用的主题
     // ref https://v2.vuepress.vuejs.org/zh/reference/config.html#theme
@@ -92,8 +92,7 @@ export default defineUserConfig({
         cache: "filesystem",
 
         /* 本地搜索, 默认启用 */
-        search: {provider: "local"},
-
+        search: { provider: "local" },
         /**
          * Algolia DocSearch
          * 启用此搜索需要将 本地搜索 search 设置为 false
@@ -130,43 +129,43 @@ export default defineUserConfig({
          * @see https://theme-plume.vuejs.press/config/markdown/
          */
         markdown: {
-        //   abbr: true,         // 启用 abbr 语法  *[label]: content
-        //   annotation: true,   // 启用 annotation 语法  [+label]: content
-        //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
-        //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
-        //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
-        //   bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
-        //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
-        //   artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
-        //   audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
-        //   icon: { provider: 'iconify' },        // 启用内置图标语法  ::icon-name::
-        //   codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
-        //   replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
-        //   codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
-        //   jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
-        //   npmTo: true,        // 启用 npm-to 容器  ::: npm-to
-        //   demo: true,         // 启用 demo 容器  ::: demo
-        //   repl: {             // 启用 代码演示容器
-        //     go: true,         // ::: go-repl
-        //     rust: true,       // ::: rust-repl
-        //     kotlin: true,     // ::: kotlin-repl
-        //   },
-        //   math: {             // 启用数学公式
-        //     type: 'katex',
-        //   },
-        //   chartjs: true,      // 启用 chart.js
-        //   echarts: true,      // 启用 ECharts
-        //   mermaid: true,      // 启用 mermaid
-        mermaid: true,
-        //   flowchart: true,    // 启用 flowchart
-        //   image: {
-        //     figure: true,     // 启用 figure
-        //     lazyload: true,   // 启用图片懒加载
-        //     mark: true,       // 启用图片标记
-        //     size: true,       // 启用图片大小
-        //   },
-        //   include: true,      // 在 Markdown 文件中导入其他 markdown 文件内容
-        //   imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
+            //   abbr: true,         // 启用 abbr 语法  *[label]: content
+            //   annotation: true,   // 启用 annotation 语法  [+label]: content
+            //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
+            //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
+            //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
+            //   bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
+            //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
+            //   artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
+            //   audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
+            //   icon: { provider: 'iconify' },        // 启用内置图标语法  ::icon-name::
+            //   codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
+            //   replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
+            //   codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
+            //   jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
+            //   npmTo: true,        // 启用 npm-to 容器  ::: npm-to
+            //   demo: true,         // 启用 demo 容器  ::: demo
+            //   repl: {             // 启用 代码演示容器
+            //     go: true,         // ::: go-repl
+            //     rust: true,       // ::: rust-repl
+            //     kotlin: true,     // ::: kotlin-repl
+            //   },
+            //   math: {             // 启用数学公式
+            //     type: 'katex',
+            //   },
+            //   chartjs: true,      // 启用 chart.js
+            //   echarts: true,      // 启用 ECharts
+            //   mermaid: true,      // 启用 mermaid
+            mermaid: true,
+            //   flowchart: true,    // 启用 flowchart
+            //   image: {
+            //     figure: true,     // 启用 figure
+            //     lazyload: true,   // 启用图片懒加载
+            //     mark: true,       // 启用图片标记
+            //     size: true,       // 启用图片大小
+            //   },
+            //   include: true,      // 在 Markdown 文件中导入其他 markdown 文件内容
+            //   imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
         },
 
         /**
@@ -180,15 +179,15 @@ export default defineUserConfig({
          * @see https://theme-plume.vuejs.press/guide/features/comments/
          */
         comment: {
-          provider: 'Giscus', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
-          comment: true,
-          repo: 'weew12/weew12.github.io',
-          repoId: 'R_kgDOO1vhMw',
-          category: 'General',
-          categoryId: 'DIC_kwDOO1vhM84CudmI',
-          mapping: 'pathname',
-          reactionsEnabled: true,
-          inputPosition: 'top',
+            provider: 'Giscus', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
+            comment: true,
+            repo: 'weew12/weew12.github.io',
+            repoId: 'R_kgDOO1vhMw',
+            category: 'General',
+            categoryId: 'DIC_kwDOO1vhM84CudmI',
+            mapping: 'pathname',
+            reactionsEnabled: true,
+            inputPosition: 'top',
         },
 
         /**
